ee1020ecd30a9a1d7a7fb93e68e30f436d892144
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-08-27 12:35:12 +0300

    PUBLISH

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sue")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+ 
+print(add(45, 55))
+    
 
 def halve(number):
-    pass
+    return number / 2
+
+print(int(4/2))
+print(float(4/2))
+
+    
